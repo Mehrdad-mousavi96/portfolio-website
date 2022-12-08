f966da1a27421e0f78b388060a604fe49ebe4848
mehrdad <mehrdad.fmousavi@gmail.com>
2022-12-08 19:23:51 +0330

    social icons added just for main page & responsive

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  return <div name='home' className="bg-slate-900 w-full h-screen">
+    This is my home page
+  </div>;
+};
 
-export default Home
+export default Home;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { HiCode } from "react-icons/hi";
-import { FaBars } from "react-icons/fa";
-import { FaTimes } from "react-icons/fa";
+import { FaBars, FaTimes, FaLinkedin, FaGithub } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -15,11 +16,11 @@ const Navbar = () => {
 
       {/* menu */}
       <ul className="hidden md:flex">
-        <li>Contact</li>
-        <li>About</li>
+        <li>Home</li>
         <li>Experience</li>
         <li>Work</li>
-        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
       </ul>
 
       {/* hamburger */}
@@ -31,16 +32,65 @@ const Navbar = () => {
       </div>
 
       {/* mobile menu */}
-      <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-slate-900 flex flex-col justify-center items-center'}>
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-slate-900 flex flex-col justify-center items-center"
+        }
+      >
         <li className="py-6 text-4xl">Home</li>
-        <li className="py-6 text-4xl">About</li>
         <li className="py-6 text-4xl">Experience</li>
         <li className="py-6 text-4xl">Work</li>
+        <li className="py-6 text-4xl">About</li>
         <li className="py-6 text-4xl">Contact</li>
       </ul>
 
       {/* social icons */}
-      <div className="hidden"></div>
+      <div className="flex fixed flex-col top-[35%] left-0">
+        <ul className="">
+          <li className="w-[140px] h-[50px] left-0 flex justify-between items-center ml-[-85px] xs:ml-[-80px] xs:hover:ml-[-5px] bg-blue-600">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              target={"_blank"}
+              href="https://www.linkedin.com/in/mehrdadmousavi-b589761bb/?locale=en_US"
+            >
+              LinkedIn
+              <FaLinkedin size={33} />
+            </a>
+          </li>
+          <li className="my-2 w-[140px] h-[50px] left-0 flex justify-between items-center ml-[-85px] xs:ml-[-80px] xs:hover:ml-[-5px] bg-black">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              target={"_blank"}
+              href="https://github.com/Mehrdad-mousavi96"
+            >
+              Github
+              <FaGithub size={33} />
+            </a>
+          </li>
+          <li className="my-2 w-[140px] h-[50px] left-0 flex justify-between items-center ml-[-85px] xs:ml-[-80px] xs:hover:ml-[-5px] bg-[#7079d3]">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              target={"_blank"}
+              href="https://github.com/Mehrdad-mousavi96"
+            >
+              Email
+              <HiOutlineMail size={33} />
+            </a>
+          </li>
+          <li className="my-2 w-[140px] h-[50px] left-0 flex justify-between items-center ml-[-85px] xs:ml-[-80px] xs:hover:ml-[-5px] bg-[#1f484c]">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              target={"_blank"}
+              href="https://github.com/Mehrdad-mousavi96"
+            >
+              Resume
+              <BsFillPersonLinesFill size={32} />
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
